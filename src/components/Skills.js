475fbe99de6b1262a0a309dd420dc130fdeffc1d
Mason Balbera <mhbalbera@gmail.com>
2023-05-17 10:16:52 -0400

    update skillz and styles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,16 @@
 function Skills(props){
 
     return(
-        <div id="Skills">
-            <p style={{ color: props.darkMode ? 'rgb(255,255,255)' : 'rgb(17,17,17)', fontWeight: 'bold', fontSize: 28, marginBottom: 75, marginLeft:10 }}>
+        <div id="Skills" style={{padding:props.isSmallScreen? 50:75}}>
+            <p style={{ color: props.darkMode ? 'rgb(255,255,255)' : 'rgb(17,17,17)', fontWeight: 'bold', fontSize: 28, marginBottom: 25, marginLeft:10 }}>
                 SKILLS
             </p>
             <div style={{ display: 'flex', justifyContent: 'space-around', flexDirection: props.isSmallScreen ? 'column' : 'row'}}>
                 <div style={{ flexDirection: 'column', marginLeft: props.isSmallScreen ? 20 : 0, marginBottom:props.isSmallScreen ? 40 : 0 }}>
                     <p style={{ fontWeight: 'bold', color: props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)' }}>LANGUAGES</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Javascript (ES6)</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Ruby</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Python</p>
+                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Ruby</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Solidity</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>HTML / CSS</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Typescript</p>
@@ -20,19 +20,21 @@ function Skills(props){
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>React (Native)</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Ruby on Rails</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Flask</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Node</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Django</p>
+                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Next.js</p>
                     <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Tailwind</p>
                 </div>
-                <div style={{ flexDirection: 'column', marginLeft: props.isSmallScreen ? 20 : 0 }}>
-                    <p style={{ fontWeight: 'bold', color: props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)' }}>TOOLS</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>AWS</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Git</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Postman</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>PostgreSQL</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>GraphQL</p>
-                    <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Chrome DevTools</p>
-                </div>
+                {!props.isSmallScreen &&
+                    <div style={{ flexDirection: 'column', marginLeft: props.isSmallScreen ? 20 : 0 }}>
+                        <p style={{ fontWeight: 'bold', color: props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)' }}>TOOLS</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>AWS</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Git</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Postman</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>PostgreSQL</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>GraphQL</p>
+                        <p style={{color:props.darkMode ? 'rgb(230,230,230)' : 'rgb(17,17,17)'}}>Chrome DevTools</p>
+                    </div>
+                }
             </div>
         </div>
     )
