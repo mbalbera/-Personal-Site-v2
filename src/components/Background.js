562da01c4ea3fc03d6fcb66c46d10e7e1b73cf14
Mason Balbera <mhbalbera@gmail.com>
2023-06-16 21:00:30 -0400

    update text

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 
 const Background = (props) => {
     const [showMore, setShowMore] = useState(false)
-    const bigText = "I am a Results-driven software engineering manager with a proven track record of leading and mentoring high-performing teams. Currently an Engineering Manager at Matter Product Studio, driving productivity and employee satisfaction through an AI certification program. Doubled engineering team size and increased revenue by addressing process inefficiencies. Skilled in JavaScript, React, Python, Ruby on Rails, AWS, and more. Let's connect to discuss how my expertise can contribute to your organization's success."
-    const smallText = "I am a results-driven software engineering manager. I have led and mentored high-performing teams. As well as built some pretty cool stuff. Let's connect to discuss how my expertise can contribute to your organization's success."
+    const bigText = "I'm Mason Balbera, a results-driven technology leader experienced in engineering management and full-stack development. As the VP of Technology at Matter Product Studio, I lead teams, uncover inefficiencies, and drive revenue growth. With a track record of establishing processes, delivering scalable solutions, and mentoring junior team members, I bring a passion for innovation and cross-functional collaboration to drive technological excellence and growth."
+    const smallText = "I'm Mason Balbera, a results-driven technology leader experienced in engineering management and full-stack development. As the VP of Technology at Matter Product Studio, I lead teams, uncover inefficiencies, and drive revenue growth."
     
     const showRightAmountOfText = (str) => {
         if(showMore){
@@ -18,10 +18,7 @@ const Background = (props) => {
     return (
         <div style={{paddingLeft:padding, paddingRight:padding, paddingTop:padding, height:'85', }}>
             <p style={{fontSize:30,color:props.darkMode? 'rgb(230,230,230)': 'rgb(17,17,17)', }}>
-                Hi! My name is
-            </p>
-            <p style={{ fontWeight: 'bold', fontSize: 36, color: props.darkMode ? 'rgb(255,255,255)' : 'rgb(35,43,59)', }}>
-                Mason Balbera.
+                Hi!
             </p>
             <p style={{fontSize:30, cursor:'pointer', color:props.darkMode? 'rgb(230,230,230)': 'rgb(17,17,17)', cursor:'default'}}>
                 {showRightAmountOfText(props.isSmallScreen? smallText:bigText)}
